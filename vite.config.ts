9615202b1bc7db5a82ba6dc63090975c1dd2a43f
SvetlanaFrolovaSvekla <savelya.105@gmail.com>
2026-09-13 02:28:26 +0600

    refactoring HomePage + added notification panel

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
@@ -41,9 +41,8 @@ export default defineConfig({
       },
     },
   },
-})
+})*/
 
-/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -66,4 +65,3 @@ export default defineConfig({
     resolve: { alias: { '@': path.resolve(__dirname, './src') } },
     server: { port: 5174, strictPort: true, host: 'localhost', proxy },
 })
-*/
